cd3a91fb2a4eabc1ead51c4db5d7d630c84b76c3
clogol <clementogol@gmail.com>
2025-06-15 12:22:13 +0300

    feat: Implement form validation and user authentication
    Implements server-side form validation and a complete authentication flow as covered in chapters 14 and 15 of the Next.js tutorial.
    
    Chapter 14: Improving Accessibility & Form Validation
    - Implemented server-side validation for the invoice forms using Zod to ensure data integrity.
    - Integrated the  hook to handle form state, capture errors, and display them to the user.
    - Enhanced form accessibility by adding ARIA attributes (, ) to associate error messages with their respective inputs.
    
    Chapter 15: Adding Authentication
    - Integrated NextAuth.js to manage user authentication, sessions, and credentials.
    - Created a custom  page and form for user sign-in.
    - Implemented Next.js Middleware to protect the  routes, redirecting unauthenticated users.
    - Added sign-in and sign-out functionality to the application.

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -3,27 +3,22 @@ import CardWrapper from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchLatestInvoices,
-        //  fetchRevenue,
-         fetchCardData,
-        } from '@/app/lib/data';
+// import {
+//          fetchCardData,
+//         } from '@/app/lib/data';
 import { Suspense } from 'react';
 import {
   RevenueChartSkeleton,
   LatestInvoicesSkeleton,
   CardsSkeleton,
 } from '@/app/ui/skeletons';
-import { revenue } from '@/app/lib/placeholder-data';
  
 export default async function Page() {
-  // const revenue = await fetchRevenue();
-  const latestInvoices = await fetchLatestInvoices();
-  const {
-    numberOfInvoices,
-    numberOfCustomers,
-    totalPaidInvoices,
-    totalPendingInvoices,
-  } = await fetchCardData();
+
+  // const {
+  //   totalPaidInvoices,
+  //   totalPendingInvoices,
+  // } = await fetchCardData();
 
   return (
     <main>
